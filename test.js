348f1287c8fcde4ac3f5c9f840dec91351edb650
hovanem <matt.hovanec@autodesk.com>
2016-02-28 09:12:35 -0800

    added logging to tests

--- test.js
+++ test.js
@@ -2,15 +2,25 @@
 'use strict';
 const supertest = require(`supertest`);
 const path = require(`path`);
-const walk = require('fs-walk');
+const walk = require(`fs-walk`);
+const winston = require('winston');
+
+const tests = [];
 const config = require(`./dist/server/config`);
 
-const location = `http://localhost:` + process.env.PORT;
-const server = supertest.agent(location);
-const tests = [];
+// Setup logger
+const filename = path.join(__dirname, `./${config.testLogFileName}`);
+const logger = new (winston.Logger)({
+  level: 'debug',
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename }),
+  ],
+});
+logger.info(`Test initialized.`);
 
 // Collect the test file from each middleware module
-walk.walkSync('./src/middleware', (basedir, filename) => {
+walk.walkSync('./dist/tests', (basedir, filename) => {
   if (filename === 'test.js') {
     const filepath = path.join(__dirname, basedir + '/' + filename);
     tests.push(require(filepath));
@@ -19,7 +29,12 @@ walk.walkSync('./src/middleware', (basedir, filename) => {
 
 // Run each middleware test
 describe('Server Tests', function middlewareTest() {
+  this.timeout(config.virtualDelay * 4);
   for (let i = 0; i < tests.length; i++) {
-    tests[i](server);
+    try {
+      tests[i]();
+    } catch (ex) {
+      logger.error(ex);
+    }
   }
 });
